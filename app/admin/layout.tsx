acf25800aa0a475181fce8efc88536d4a93de27b
Manozsen <maanozsen@gmail.com>
2026-04-21 19:35:09 +0530

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,6 @@
 import AdminLayout from "@/components/admin/AdminLayout";
 
-export const metadata = {
-  title: "Admin Panel — Manoz's Portfolio",
-};
+export const metadata = { title: "Admin Panel — Manoz" };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <AdminLayout>{children}</AdminLayout>;
