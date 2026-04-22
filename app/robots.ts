ed3e9c9ac68da2656a8ee22a6c49ad4d36003917
Manozsen <maanozsen@gmail.com>
2026-04-22 22:10:33 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://manoz-portfolio.vercel.app";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  "https://manoz-portfolio-546n.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
