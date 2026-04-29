42f3ca4b0154a254da7a1e09e1f8fc3e5b67428f
Manozsen <maanozsen@gmail.com>
2026-04-29 08:24:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      fontFamily: {},
+    },
   },
   plugins: [],
 };
